980caf0efcf332d31bd07dce04b20b816616db93
alex-koshara <alex-koshara@mail.ru>
2016-12-18 00:12:37 +0300

    add mainNav

---
+++ markup/components/mainNav/data/data.js
@@ -0,0 +1,20 @@
+exports = {
+  mainNav: [
+    {
+      link: "index.html",
+      text: "Главная"
+    },
+    {
+      link: "photo.html",
+      text: "Фотографии"
+    },
+    {
+      link: "contest.html",
+      text: "Конкурс"
+    },
+    {
+      link: "https://htmlacademy.ru/",
+      text: "HTML Academy"
+    }
+  ]
+};
