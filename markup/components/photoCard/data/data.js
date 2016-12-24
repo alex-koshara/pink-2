d70c4266acd055a24136ef40c022098438dce079
alex-koshara <alex-koshara@mail.ru>
2016-12-24 18:04:18 +0300

    add photo Card

---
+++ markup/components/photoCard/data/data.js
@@ -0,0 +1,11 @@
+exports = {
+  photoCard: [
+    {
+      img: "../static/img/content/photo-1",
+      userName: "Левон",
+      timePub: "5 часов назад",
+      desc: "Вчера он на луну летал, сегодня в руки к нам попал.",
+      like: "356"
+    }
+  ]
+};
