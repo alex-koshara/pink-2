760e6763dcd16e86ed46edeefa8d1476fec17f49
alex-koshara <alex-koshara@mail.ru>
2016-12-26 22:30:52 +0300

    add social icon

---
+++ markup/components/socialIcon/data/data.js
@@ -0,0 +1,16 @@
+exports = {
+  icons: [
+    {
+      icon: "twitter",
+      link: "http://twitter.com"
+    },
+    {
+      icon: "facebook",
+      link: "http://facebook.com"
+    },
+    {
+      icon: "youtube",
+      link: "http://youtube.com"
+    }
+  ]
+};
