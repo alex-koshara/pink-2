b004169a96d600c0bbd0b09586491887aadb4e54
alex-koshara <alex-koshara@mail.ru>
2017-01-07 19:46:40 +0300

    add logo

---
+++ markup/components/logo/data/data.js
@@ -0,0 +1,11 @@
+exports = {
+  logo: [
+    {
+      mobile: "../static/img/svg/logo-pink-white-mobile.svg",
+      tablet: "../static/img/svg/logo-pink-white-tablet.svg",
+      desktop: "../static/img/svg/logo-pink-white-desktop.svg",
+      mobileBlue: "../static/img/svg/logo-pink-blue-mobile.svg",
+      desktopBlue: "../static/img/svg/logo-pink-blue-desktop.svg"
+    }
+  ]
+};
