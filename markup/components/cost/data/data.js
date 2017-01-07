c354b6d2d373f512c9550a7610034ac5a27a3eff
alex-koshara <alex-koshara@mail.ru>
2017-01-07 12:06:58 +0300

    add cost

---
+++ markup/components/cost/data/data.js
@@ -0,0 +1,28 @@
+exports = {
+  cost: [
+    {
+      service: "База",
+      price: "1.99 USD"
+    }
+  ],
+  services: [
+    {
+      name: "Розовый фильтр",
+      base: true,
+      standart: true,
+      unlim: true
+    },
+    {
+      name: "Смайлики",
+      base: false,
+      standart: true,
+      unlim: true
+    },
+    {
+      name: "Комментарии",
+      base: false,
+      standart: false,
+      unlim: true
+    }
+  ]
+};
